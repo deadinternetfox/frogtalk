67d1cfc32fac784cacfdd4a11f25fbb10b0f6ce3
deadinternetfox <deadinternetfox@users.noreply.github.com>
2026-05-04 01:55:56 +1200

    .

--- static/js/app.js
+++ static/js/app.js
@@ -5,7 +5,7 @@
 const App = {
   pendingInvite: null,  // Store invite code to process after login
   PENDING_CALL_KEY: 'ft_pending_incoming_call',
-  ASSET_RESET_VERSION: 'loader-skeleton-hotfix-v1',
+  ASSET_RESET_VERSION: 'reaction-panel-click-hotfix-v1',
   easterEgg: null,
   easterTapCount: 0,
   easterTapTimer: null,
